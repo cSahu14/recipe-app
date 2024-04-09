df96c2df8b692dcde182533c7b08b18cbd63c4a6
cSahu14 <sahuchidananda1999@gmail.com>
2024-04-09 19:55:44 +0530

    build file name change

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const client = new ApolloClient({
 	// TODO => Update the uri on production
-	uri: import.meta.env.NODE_ENV === "development" ? "http://localhost:4000/graphql" : "/graphql",
+	uri: process.env.NODE_ENV === "development" ? "http://localhost:4000/graphql" : "/graphql",
 	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
 	credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
 });
